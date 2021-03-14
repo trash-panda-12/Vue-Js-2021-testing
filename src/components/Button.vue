df3d02c664f3066029a5f81679b1aad68599d151
David <davidphilipsumner@gmail.com>
2021-03-14 12:26:13 +0000

    Initial Commit. Create task components

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick()" v-bind:style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('click')
+    },
+  },
+}
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <h3>
+      <Task :task="task" />
+    </h3>
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  name: 'tasks',
+  props: {
+    tasks: Array,
+  },
+
+  components: {
+    Task,
+  },
+}
+</script>
